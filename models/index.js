61f4e7b4a3590c06afd7ad33792140f76e689258
bokaderohit98 <bokaderohit98@gmail.com>
2020-03-07 17:16:53 +0530

    models: created index file

---
+++ models/index.js
@@ -0,0 +1,11 @@
+const User = require("./User");
+const Education = require("./Education");
+const Work = require("./Work");
+const Achivement = require("./Achivement");
+
+module.exports = {
+  User,
+  Education,
+  Work,
+  Achivement
+};
