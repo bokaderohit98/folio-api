ee36d6478feaaf5a63ebbb96ee37c86e5e5d081d
bokaderohit98 <bokaderohit98@gmail.com>
2020-03-08 14:40:02 +0530

    cleanup: refactoring

--- helpers/validations.js
+++ helpers/validations.js
@@ -9,7 +9,7 @@ exports.validateEmail = email => {
   const emailRegExp = RegExp(
     /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
   );
-  return emailRegExp.test(email);
+  return emailRegExp.test(email.trim());
 };
 
 /**
@@ -18,12 +18,9 @@ exports.validateEmail = email => {
  * @returns {Object} - containing request body and error object
  */
 exports.validateLoginDetails = ({ email, password, otp }, type) => {
-  const emailRegExp = RegExp(
-    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-  );
   let errorMessage = "";
 
-  if (!email || !email.trim() || !emailRegExp.test(email.trim()))
+  if (!email || !email.trim() || !exports.validateEmail(email))
     errorMessage = "Invalid email";
   else if (type === "password" && (!password || !password.trim()))
     errorMessage = "Invalid password";
@@ -53,14 +50,11 @@ exports.validateBasicDetails = ({
   gender,
   social
 }) => {
-  const emailRegExp = RegExp(
-    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-  );
   let errorMessage = "";
 
   if (!name || !name.trim()) errorMessage = "Missing name";
-  else if (!email || !email.trim() || !emailRegExp.test(email.trim()))
-    errorMessage = "Missing or invalid email";
+  if (!email || !email.trim() || !exports.validateEmail(email))
+    errorMessage = "Invalid email";
   else if (!password || !password.trim()) errorMessage = "Missing Password";
   else if (!dob || !dob.trim())
     errorMessage = "Missing or invalid Date of Birth";
